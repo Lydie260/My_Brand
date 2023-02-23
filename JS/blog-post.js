e59652b0d16e09fda85acb35ea6a5f46f003a6bf
lydie260 <gatarelydie370@gmail.com>
2023-02-23 16:51:53 +0200

    implentation of adding a picture in blog form

--- JS/blog-post.js
+++ JS/blog-post.js
@@ -1,22 +1,23 @@
 const form = document.querySelector("form");
 let image = "";
 
-// document.getElementById("img").addEventListener("change", (e) => {
-//   const file = e.target.files[0];
-//   const reader = new FileReader();
-//   reader.onloadend = () => {
-//     const base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
-//     image = base64String;
-//   };
-//   reader.readAsDataURL(file);
-// });
+document.getElementById("img").addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    const base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+    image = base64String;
+  };
+  reader.readAsDataURL(file);
+});
 const createNewBlog = async (e) => {
   e.preventDefault();
   const temp = {
     title: form.title.value,
     author: form.author.value,
-   datePosted: form.datePosted.value,
     description: form.description.value,
+    datePosted: form.datePosted.value,
+    img:image,
   
   };
   console.log(temp)
